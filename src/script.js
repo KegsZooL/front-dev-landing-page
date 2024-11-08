f1f2fc01f975b0f146081b0c197b75eb0a48def6
KegsZooL <danil.chistyakov.2015@inbox.ru>
2024-11-08 17:36:47 +0300

    correted the resolution

--- src/script.js
+++ src/script.js
@@ -1,12 +1,31 @@
-document.querySelectorAll('.spoiler-toggle-button').forEach(
-  button => {
-    button.addEventListener('click', () => {
-      const spoiler = button.closest('.spoiler');
-      const spoilerContent = spoiler.querySelector('.spoiler-content');
-      const isOpen = spoilerContent.style.display === 'flex';
-      
-      spoilerContent.style.display = isOpen ? 'none' : 'flex';
-      button.textContent = isOpen ? '+' : '—';
-    })
-  }
+document.querySelectorAll('.spoiler-toggle-button').forEach(button => {
+  button.addEventListener('click', () => {
+    const spoiler = button.closest('.spoiler');
+    const spoilerContent = spoiler.querySelector('.spoiler-content');
+    
+    spoilerContent.classList.toggle('show');
+    
+    const isOpen = spoilerContent.classList.contains('show');
+    button.textContent = isOpen ? '—' : '+';
+  });
+});
+
+// function scrollToSection() {
+//   const element = document.getElementById("target-audience");
+//   const offset = 500;
+
+//   window.scrollTo({
+//       top: element.getBoundingClientRect().top + window.scrollY + offset,
+//       behavior: "smooth"
+//   });
+// }
+
+document.querySelectorAll('.smooth-goto').forEach(href => {
+  const offset = 50;
+    window.scrollTo({
+      top: href.getBoundingClientRect().top + window.scrollY + offset,
+      behavior: "smooth"
+  });
+}
 )
+
